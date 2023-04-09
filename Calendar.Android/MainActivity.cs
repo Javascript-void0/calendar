5bbb5003e198568a49765d8af90ede8010f4ecfd
Java <indead1124@gmail.com>
2023-04-09 01:54:21 -0400

    Update MainActivity.cs

--- Calendar.Android/MainActivity.cs
+++ Calendar.Android/MainActivity.cs
@@ -4,6 +4,7 @@ using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
 using Android.OS;
+using Android.Views;
 
 namespace Calendar.Droid
 {
@@ -12,14 +13,17 @@ namespace Calendar.Droid
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
-            base.OnCreate(savedInstanceState);
+			base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new App());
+
+			LoadApplication(new App());
             Window.SetStatusBarColor(Android.Graphics.Color.Argb(255, 32, 32, 32));
+            Window.SetNavigationBarColor(Android.Graphics.Color.Argb(255, 32, 32, 32));
         }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+
+		public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
